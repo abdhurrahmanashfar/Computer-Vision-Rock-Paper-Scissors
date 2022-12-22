b5c76ab9e1c536cd5eafbbf26c381063569250d5
abdhurrahmanashfar <117317215+abdhurrahmanashfar@users.noreply.github.com>
2022-12-23 01:09:04 +0530

    Add files via upload

---
+++ manual_rps.py
@@ -0,0 +1,36 @@
+import random
+
+#word_list = ["rock", "paper", "scissors"]
+
+def get_computer_choice():
+    word_list = ["rock", "paper", "scissors"]
+    computer_choice = random.choice(word_list)
+    return computer_choice
+
+
+def get_user_choice():
+    user_choice = input("Rock, Paper, Scissors: ").lower()
+    return user_choice
+
+def get_winner(computer_choice, user_choice):
+    #self.computer_choice = computer_choice
+    #self.user_choice = user_choice
+    
+    if computer_choice == user_choice:
+        print('It is a tie!')
+    elif (
+        (computer_choice == 'rock' and user_choice == 'scissors') or
+        (computer_choice == 'paper' and user_choice == 'rock') or
+        computer_choice == 'scissors' and user_choice == 'paper'
+        ):
+        print('You lost!')
+    else:
+        print('You won!')
+
+def play():
+    computer_choice = get_computer_choice()
+    user_choice = get_user_choice()
+    winner = get_winner(computer_choice, user_choice)
+
+
+play()
