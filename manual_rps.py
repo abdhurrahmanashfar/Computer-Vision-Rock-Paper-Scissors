d751747ec4f39dc2d5611b07888c254788ac3124
abdhurrahmanashfar <117317215+abdhurrahmanashfar@users.noreply.github.com>
2022-12-23 00:59:45 +0530

    Update manual_rps.py

--- manual_rps.py
+++ manual_rps.py
@@ -12,25 +12,26 @@ def get_user_choice():
     user_choice = input("Rock, Paper, Scissors: ").lower()
     return user_choice
 
-def get_winner(computer_choice, user_choice):
+
+#def get_winner(computer_choice, user_choice):
     #self.computer_choice = computer_choice
     #self.user_choice = user_choice
     
-    if computer_choice == user_choice:
+ #   if computer_choice == user_choice:
         print('It is a tie!')
-    elif (
-        (computer_choice == 'rock' and user_choice == 'scissors') or
-        (computer_choice == 'paper' and user_choice == 'rock') or
-        computer_choice == 'scissors' and user_choice == 'paper'
-        ):
-        print('You lost!')
-    else:
-        print('You won!')
+  #  elif (
+   #     (computer_choice == 'rock' and user_choice == 'scissors') or
+    #    (computer_choice == 'paper' and user_choice == 'rock') or
+     #   computer_choice == 'scissors' and user_choice == 'paper'
+      #  ):
+       # print('You lost!')
+    #else:
+     #   print('You won!')
 
-def play():
-    computer_choice = get_computer_choice()
-    user_choice = get_user_choice()
-    winner = get_winner(computer_choice, user_choice)
+#def play():
+ #   computer_choice = get_computer_choice()
+  #  user_choice = get_user_choice()
+   # winner = get_winner(computer_choice, user_choice)
 
 
-play()
+#play()
